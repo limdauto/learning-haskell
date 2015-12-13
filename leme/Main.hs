a2a44ce5329e487984eb4a2ed484718f0ccd4c58
Lim H <limdauto@gmail.com>
2015-12-14 00:02:02 +0700

    Initialize leme project

---
+++ leme/Main.hs
@@ -0,0 +1,54 @@
+module Main where
+
+import Control.Monad
+import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+parseString :: Parser LispVal
+parseString = do
+                char '"'
+                x <- many (noneOf "\"")
+                char '"'
+                return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+                first <- letter <|> symbol
+                rest <- many (letter <|> symbol <|> digit)
+                let atom = first:rest
+                return $ case atom of
+                        "#t" -> Bool True
+                        "#f" -> Bool False
+                        _    -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+         <|> parseString
+         <|> parseNumber
+
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+                    Left err -> "No match: " ++ show err
+                    Right err -> "Found value"
+
+main :: IO ()
+main = do
+        (expr:_) <- getArgs
+        putStrLn (readExpr expr)
