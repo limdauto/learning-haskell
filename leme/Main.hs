a71578aa213a520d5ca6258af5ad36d6d263dd48
Lim H <limdauto@gmail.com>
2015-12-15 06:17:00 +0700

    Initiate leme project and finish adding a parser

--- leme/Main.hs
+++ leme/Main.hs
@@ -10,6 +10,7 @@ data LispVal = Atom String
              | Number Integer
              | String String
              | Bool Bool
+    deriving Show
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
@@ -37,11 +38,38 @@ parseAtom = do
 parseNumber :: Parser LispVal
 parseNumber = liftM (Number . read) $ many1 digit
 
+parseList :: Parser LispVal
+parseList = liftM List $ sepBy parseExpr spaces
+
+-- endBy p sep parses zero or more occurrences of p, separated and ended by sep. Returns a list of values returned by p.
+-- https://hackage.haskell.org/package/parsec-3.1.9/docs/src/Text-Parsec-Combinator.html#endBy
+
+parseDottedList :: Parser LispVal
+parseDottedList = do
+    head <- endBy parseExpr spaces
+    tail <- char '.' >> spaces >> parseExpr
+    return $ DottedList head tail
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+    char '\''
+    x <- parseExpr
+    return $ List [Atom "quote", x]
+
+-- http://blog.ezyang.com/2014/05/parsec-try-a-or-b-considered-harmful/
+-- http://stackoverflow.com/questions/20020350/parsec-difference-between-try-and-lookahead
+-- related:
+--   https://hackage.haskell.org/package/parsec-3.1.9/docs/src/Text-Parsec-Combinator.html#option
+--   http://stackoverflow.com/questions/20020350/parsec-difference-between-try-and-lookahead
 parseExpr :: Parser LispVal
 parseExpr = parseAtom
          <|> parseString
          <|> parseNumber
-
+         <|> parseQuoted
+         <|> do char '('
+                x <- try parseList <|> parseDottedList
+                char ')'
+                return x
 
 readExpr :: String -> String
 readExpr input = case parse parseExpr "lisp" input of
