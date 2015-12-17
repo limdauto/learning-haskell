05d5d3800899e6f8e4bd79e5567470b061c3e37d
Lim H <limdauto@gmail.com>
2015-12-17 11:28:20 +0700

    Modularize the package

---
+++ leme/LispError.hs
@@ -0,0 +1,24 @@
+module LispError where
+
+import Control.Monad.Except
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+import LispData
+
+data LispError = NumArgs Integer [LispVal]
+               | TypeMismatch String LispVal
+               | Parser ParseError
+               | BadSpecialForm String LispVal
+               | NotFunction String String
+               | UnboundVar String String
+               | Default String
+
+instance Show LispError where
+    show (UnboundVar message varname)  = message ++ ": " ++ varname
+    show (BadSpecialForm message form) = message ++ ": " ++ show form
+    show (NotFunction message func)    = message ++ ": " ++ show func
+    show (NumArgs expected found)      = "Expected " ++ show expected
+                                            ++ " args; found values " ++ unwordsList found
+    show (TypeMismatch expected found) = "Invalid type: expected " ++ expected
+                                            ++ ", found " ++ show found
+    show (Parser parseErr)             = "Parse error at " ++ show parseErr
--- leme/Main.hs
+++ leme/Main.hs
@@ -4,24 +4,8 @@ import Control.Monad
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
 
-data LispVal = Atom String
-             | List [LispVal]
-             | DottedList [LispVal] LispVal
-             | Number Integer
-             | String String
-             | Bool Bool
-
-instance Show LispVal where
-    show (String contents) = "\"" ++ contents ++ "\""
-    show (Atom name) = name
-    show (Number contents) = show contents
-    show (Bool True) = "#t"
-    show (Bool False) = "#f"
-    show (List contents) = "(" ++ unwordsList contents ++ ")"
-    show (DottedList x xs) = "(" ++ unwordsList x ++ " . " ++ show xs ++ ")"
-
-unwordsList :: [LispVal] -> String
-unwordsList = unwords . map show
+import LispData
+import LispError
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
