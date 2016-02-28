6a72867a20c11ffe3cf457bf1edf870ffbe7fd74
Lim H <limdauto@gmail.com>
2016-02-28 23:56:06 +0000

    Wrong method

---
+++ hacker-rank/Sherlocks.hs
@@ -0,0 +1,10 @@
+import Control.Monad
+
+main = readLn >>= \t ->
+    forM_ [1..t] $ \_ ->
+    readLn >>= putStrLn . solveFor
+
+notDivBy3 = (/= 0) . (`mod` 3)
+solveFor n = case dropWhile notDivBy3 [n, n-5, maximum [n-10, -1]] of
+                (n5:_) -> replicate n5 '5' ++ replicate (n-n5) '3'
+                []     -> "-1"
