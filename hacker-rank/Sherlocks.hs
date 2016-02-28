fc210a4e977eccee780d960b3e4a2479693d91a4
Lim H <limdauto@gmail.com>
2016-02-28 23:13:00 +0000

    Cosmetic changes

--- hacker-rank/Sherlocks.hs
+++ hacker-rank/Sherlocks.hs
@@ -1,14 +1,10 @@
 import Control.Monad
 
-main :: IO ()
-main = do
-    numTests <- getLine
-    let t = read numTests :: Int
-    forM_ [1..t] $ \_ -> do
-        numDigits <- getLine
-        let n = read numDigits :: Int
-        output n $ dropWhile notDivBy3 [n,n-5..(-1)]
-    where notDivBy3 = (/= 0) . (`mod` 3)
-          output n (n5:_) = putStrLn $ replicate n5 '5' ++ replicate (n-n5) '3'
-          output _ [] = putStrLn "-1"
+main = readLn >>= \t ->
+    forM_ [1..t] $ \_ ->
+    readLn >>= putStrLn . solveFor
 
+notDivBy3 = (/= 0) . (`mod` 3)
+solveFor n = case dropWhile notDivBy3 [n,n-5..(-1)] of
+                (n5:_) -> replicate n5 '5' ++ replicate (n-n5) '3'
+                []     -> "-1"
