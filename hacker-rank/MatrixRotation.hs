368aaca4bf1bd8912a01ced14eb4b0041fe73d1c
Lim H <limdauto@gmail.com>
2016-03-05 22:49:06 +0000

    BOOM Matrix rotation

--- hacker-rank/MatrixRotation.hs
+++ hacker-rank/MatrixRotation.hs
@@ -1,46 +1,111 @@
+{-# LANGUAGE NamedFieldPuns #-}
 import Control.Monad
 import Data.List
 
 
-type Depth = Int
 data Array2D = Array2D [[Int]]
-data Size = Size Int Int
-data Position = Position Int Int
-data Matrix = Matrix Size Array2D
 
+data Size = Size {
+    rows :: Int,
+    cols :: Int
+}
+
+data Position = Position {
+    x :: Int,
+    y :: Int
+}
+
+data BoundingRect = BoundingRect {
+    topLeft :: Position,
+    topRight :: Position,
+    bottomLeft :: Position,
+    bottomRight :: Position
+}
+
+data Matrix = Matrix {
+    size :: Size,
+    nums :: Array2D
+}
 
 instance Show Matrix where
-    show (Matrix (Size rows cols) nums) = "Rows: " ++ show rows ++ " | Cols: " ++ show cols ++ "\n" ++ show nums
+    show m = "Rows: " ++ (show . rows . size $ m) ++ " | Cols: " ++ (show . cols . size $ m) ++ "\n" ++ (show . nums $ m)
 
 instance Show Array2D where
-    show (Array2D nums) = intercalate "\n" . map (intercalate " " . map show) $ nums
+    show (Array2D arr) = intercalate "\n" . map (intercalate " " . map show) $ arr
 
+instance Show Position where
+    show pos = "(" ++ (intercalate ", " . map show $ [x pos, y pos]) ++ ")"
+
+instance Show BoundingRect where
+    show box = "Top Left: " ++ (show . topLeft $ box) ++ " | Top Right: " ++ (show . topRight $ box) ++
+               " | Bottom Left: " ++ (show . bottomLeft $ box) ++ " | Bottom Right: " ++ (show . bottomRight $ box) ++ "\n"
+
+instance Eq Position where
+    (Position x1 y1) == (Position x2 y2) = x1 == x2 && y1 == y2
 
 main :: IO ()
 main = do
     nTemp <- getLine
     let (m:n:r:_) = map read $ words nTemp :: [Int]
-    nums <- foldM readLines (Array2D []) [1..m]
-    let matrix = Matrix (Size m n) nums
-    print $ rotate matrix r
+    arr <- foldM readLines (Array2D []) [1..m]
+    let matrix = Matrix (Size m n) arr
+    let results = rotate matrix r
+    putStrLn $ intercalate "\n" [intercalate " " [showJust $ lookup (Position x y) results |
+                                 x <- [1..n]] | y <- [1..m]]
 
+showJust :: Maybe Int -> String
+showJust (Just x) = show x
 
 readLines :: Array2D -> t -> IO Array2D
-readLines (Array2D nums) _ = do
+readLines (Array2D arr) _ = do
     arrTemp <- getLine
-    return $ Array2D (nums ++ [(map read $ words arrTemp :: [Int])])
-
-
--- backtrack a final position in a block to the current position
-backtrack :: Size -> Position -> Int -> Position
-backtrack (Size rows cols) (Position finalX finalY) numRotations_ = undefined
-    where numRotations = numRotations_ `mod` ((rows + cols) * 2)
-
-
-rotate :: Matrix -> Int -> Matrix
-rotate m@(Matrix size@(Size cols rows) (Array2D nums)) numRotations = Matrix size rotated
-    where rotated = Array2D [[elemAt m $ (backtrack size (Position col row) numRotations) | col <- [1..cols]] | row <- [1..rows]]
+    return $ Array2D (arr ++ [(map read $ words arrTemp :: [Int])])
 
 
 elemAt :: Matrix -> Position -> Int
-elemAt (Matrix _ (Array2D nums)) (Position x y) = nums !! (x - 1) !! (y - 1)
+elemAt (Matrix _ (Array2D nums)) (Position x y) = nums !! (y - 1) !! (x - 1)
+
+
+onLeftEdge box pos = (x . topLeft $ box) == (x pos)
+onRightEdge box pos = (x . topRight $ box) == (x pos)
+onTopEdge box pos = (y . topLeft $ box) == (y pos)
+
+
+backtrackOne :: BoundingRect -> Position -> Position
+backtrackOne box@(BoundingRect {topLeft, topRight, bottomLeft, bottomRight}) pos@(Position {x, y})
+    | pos == topLeft        = Position (x + 1) y
+    | pos == topRight       = Position x (y + 1)
+    | pos == bottomLeft     = Position x (y - 1)
+    | pos == bottomRight    = Position (x - 1) y
+    | onLeftEdge box pos    = Position x (y - 1)
+    | onRightEdge box pos   = Position x (y + 1)
+    | onTopEdge box pos     = Position (x + 1) y
+    | otherwise             = Position (x - 1) y
+
+
+backtrack :: Matrix -> BoundingRect -> Int -> Position -> (Position, Int)
+backtrack matrix box numRotations pos = let origin = foldl' (\p _ -> backtrackOne box p) pos [1..numRotations] in (pos, elemAt matrix origin)
+
+
+boundingBoxes :: Size -> [BoundingRect]
+boundingBoxes (Size rows cols) = [BoundingRect (topLeft d) (topRight d) (bottomLeft d) (bottomRight d) | d <- [0..numBoxes - 1]]
+    where numBoxes          = minimum [rows, cols] `div` 2
+          topLeft depth     = Position (1 + depth) (1 + depth)
+          topRight depth    = Position (cols - depth) (1 + depth)
+          bottomLeft depth  = Position (1 + depth) (rows - depth)
+          bottomRight depth = Position (cols - depth) (rows - depth)
+
+
+walkOnBoundingBox :: BoundingRect -> [Position]
+walkOnBoundingBox box = nub $ walkOnLeftEdge ++ walkOnBottomEdge ++ walkOnRightEdge ++ walkOnTopEdge
+    where walkOnLeftEdge = zipWith Position (repeat . x . topLeft $ box) [(y . topLeft $ box)..(y . bottomLeft $ box)]
+          walkOnRightEdge = zipWith Position (repeat . x . topRight $ box) [(y . topRight $ box)..(y . bottomRight $ box)]
+          walkOnTopEdge = zipWith Position [(x . topLeft $ box)..(x . topRight $ box)] (repeat . y . topLeft $ box)
+          walkOnBottomEdge = zipWith Position [(x . bottomLeft $ box)..(x . bottomRight $ box)] (repeat . y . bottomLeft $ box)
+
+
+rotate :: Matrix -> Int -> [(Position, Int)]
+rotate matrix numRotations =  concat $ map backtrackOneBox boxes
+    where boxes = boundingBoxes . size $ matrix
+          backtrackOneBox box = map (backtrack matrix box numRotations) (walkOnBoundingBox box)
+
