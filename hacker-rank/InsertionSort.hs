24862e4922d3cccfb3c5e6d93af4a73dc0dcb3de
Lim H <limdauto@gmail.com>
2016-03-06 19:27:21 +0000

    Way better solution

--- hacker-rank/InsertionSort.hs
+++ hacker-rank/InsertionSort.hs
@@ -3,8 +3,8 @@ import Control.Monad.Error
 import Control.Monad.Trans.Either
 import Data.List
 
-main :: IO ()
-main = do
+main' :: IO ()
+main' = do
     temp <- getLine
     let size = read temp :: Int
     arrTemp <- getLine
@@ -18,3 +18,19 @@ main = do
         lift $ putStrLn $ intercalate " " $ map show (reverse output)
         when (length second == 0 || e > (head second)) $ left ()
     return ()
+
+-- better solution: https://codepair.hackerrank.com/paper/TkSezB72?b=eyJyb2xlIjoiY2FuZGlkYXRlIiwibmFtZSI6ImxpbWRhdXRvIiwiZW1haWwiOiJsaW1kYXV0b0BnbWFpbC5jb20ifQ%3D%3D
+main :: IO ()
+main = do
+    size <- liftM read getLine
+    (numbers, (v : _)) <- liftM (splitAt (size - 1) . map read . words) getLine
+    mapM_ printList $ ins v (reverse numbers :: [Int])
+
+ins :: Ord a => a -> [a] -> [[a]]
+ins v [] = [[v]]
+ins v (x:xs)
+    | x > v = (x:x:xs) : (map ((:) x) $ ins v xs)
+    | otherwise = [(v : x : xs)]
+
+printList :: Show a => [a] -> IO ()
+printList = putStrLn . unwords . map show . reverse
