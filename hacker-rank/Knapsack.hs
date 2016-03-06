4958fc824ed6050b5db72993e6aca50f25f27096
Lim H <limdauto@gmail.com>
2016-03-06 22:07:47 +0000

    Optimize

--- hacker-rank/Knapsack.hs
+++ hacker-rank/Knapsack.hs
@@ -14,10 +14,9 @@ parseInts = (map read) . words
 knapsack :: [Int] -> Int -> [Int]
 knapsack []             _ = []
 knapsack numbers@(x:xs) k
-    | x > k     = knapsack xs k
+    | x > k             = knapsack xs k
+    | k `mod` x == 0    = replicate (k `div` x) x
     | otherwise = merge withX withoutX
     where withX = x : (knapsack numbers (k - x))
           withoutX = knapsack xs k
           merge a b = if sum a > sum b then a else b
-
-
