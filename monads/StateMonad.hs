5904610b05e944fbf676253d8a3422552a58624f
Lim H <limdauto@gmail.com>
2016-03-10 23:27:47 +0000

    Functor and applicative instance for state

--- monads/StateMonad.hs
+++ monads/StateMonad.hs
@@ -1,7 +1,17 @@
 module StateMonad where
 
-newtype State s a = State { runState :: (s -> (a,s)) }
+newtype State s a = State { runState :: s -> (a,s) }
+
+instance Functor (State s) where
+    fmap f (State x) = State $ \s -> let (v, s') = x s in (f v, s')
+
+instance Applicative (State s) where
+    pure a = State $ \s -> (a, s)
+    (State f) <*> (State a) = State $ \s -> let (f', s1) = f s
+                                                (a', s') = a s1
+                                            in (f' a', s')
 
 instance Monad (State s) where
     return a        = State $ \s -> (a,s)
-    (State x) >>= f = State $ \s -> let (v,s') = x s in runState (f v) s'
+    (State x) >>= f = State $ \s -> let (v, s') = x s in runState (f v) s'
+
