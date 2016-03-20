440b0c41590e3e35538ee54bdbd41d9a918b31b6
Lim H <limdauto@gmail.com>
2016-03-20 22:32:01 +0000

    Trying to find an application for Free

--- monads/FreeMonad.hs
+++ monads/FreeMonad.hs
@@ -3,17 +3,6 @@ module FreeMonad where
 -- http://www.haskellforall.com/2012/06/you-could-have-invented-free-monads.html
 -- http://dlaing.org/cofun/posts/free_and_cofree.html
 -- http://arxiv.org/pdf/1403.0749v3.pdf
-import Control.Monad.Fix ()
-
-data Toy b next = Output b next
-                | Bell next
-                | Done
-data FixE f e = Fix (f (FixE f e)) | Throw e
-
-catch :: (Functor f) => FixE f e1 -> (e1 -> FixE f e2) -> FixE f e2
-catch (Fix x) f = Fix (fmap (`catch` f) x)
-catch (Throw e) f = f e
-
 data Free f a = Pure a
               | Free (f (Free f a))
 
@@ -30,3 +19,27 @@ instance Functor f => Monad (Free f) where
     return x = Pure x
     (Pure r) >>= f = f r
     (Free x) >>= f = Free (fmap (>>= f) x)
+
+-- Application 1: option parsers
+data User = User {
+    username :: String,
+    fullname :: String,
+    id :: Int
+} deriving Show
+
+data Option a = Option {
+    optName :: String,
+    optDefault :: Maybe a,
+    optReader :: String -> Maybe a
+} deriving Functor
+
+readInt :: String -> Int
+readInt x = read x :: Int
+
+userP :: Free Option User
+userP = User <$> one (Option "username" Nothing Just)
+             <*> one (Option "fullname" (Just "") Just)
+             <*> one (Option "id" Nothing readInt)
+
+one :: Option a -> Free Option a
+one opt = Pure opt
